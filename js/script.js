98fa10ce7dbaef0572d778832f4dd2fbfeb1720a
N'Guessan Josue Emmanuel Moro III <your-email@example.com>
2026-09-22 22:49:33 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// =========================================
+// WEBEMMa DIGITAL STUDIO
+// Main JavaScript
+// =========================================
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("WEBEMMa Digital Studio loaded.");
+
+});
+
